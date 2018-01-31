0c8cd65389a7f5e44070dbfeaf3d239ba133ca76
Thomas Geisbacher <thomas.geisbacher@gmail.com>
2018-01-31 15:19:31 +0100

    created new pkg svdrp and func collectDataFromServer

---
+++ svdrp/svdrp.go
@@ -0,0 +1,49 @@
+package svdrp
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func collectDataFromServer(addr, command string) (string, error) {
+
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return "", fmt.Errorf("error while connecting to server '%s', error: %v", addr, err)
+	}
+	defer conn.Close()
+
+	// reading welcome-message from server
+	fromServReader := bufio.NewReader(conn)
+	_, err = fromServReader.ReadString('\n')
+	if err != nil {
+		return "", fmt.Errorf("could not read welcome-message from server, error: %v", err)
+	}
+
+	// sending command
+	fmt.Fprint(conn, command)
+
+	// receiving answer from server
+	response := ""
+	s := bufio.NewScanner(fromServReader)
+	s.Split(bufio.ScanLines)
+	for s.Scan() {
+		line := s.Text()
+		response = response + line
+		if isLastLine(line) == true {
+			break
+		}
+	}
+
+	return response, nil
+}
+
+func isLastLine(line string) bool {
+	if strings.Index(line, "-") == 3 {
+		return false
+	}
+
+	return true
+}
--- svdrp/svdrp_test.go
+++ svdrp/svdrp_test.go
@@ -1,4 +1,4 @@
-package main
+package svdrp
 
 import "testing"
 
