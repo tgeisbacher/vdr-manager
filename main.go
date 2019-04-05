0050cea95d31533b9599e281b3640d8abee98109
Thomas Geisbacher <thomas.geisbacher@gmail.com>
2019-04-05 12:05:13 +0200

    refactored API

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"github.com/tgeisbacher/vdr-manager/svdrp"
 )
 
@@ -24,61 +28,56 @@ func main() {
 		fmt.Println("Showname: ", data.ShowName)
 		fmt.Println("Starttime:", data.StartTime, "Duration:", data.Duration)
 		fmt.Println("Subtitle: ", data.Subtitle)
-		fmt.Println("")
-
 	}
 	_, err = svdrp.ListEPGNow("vdr.dd:6419")
 	if err != nil {
 		fmt.Println("ERROR:", err)
 	}
+	
+	fs := http.FileServer(http.Dir("html"))
+	router := mux.NewRouter().StrictSlash(true)
+	router.Methods("GET").Path("/api/channels").HandlerFunc(apiChannelsHandler) 
+	router.Methods("GET").Path("/api/epg").HandlerFunc(apiEPGHandler)          
+	router.PathPrefix("/").Handler(fs)
+	handler := cors.Default().Handler(router)
 
-	http.HandleFunc("/", startPageHandler)
-	http.HandleFunc("/webshop", webshopHandler)
-	err = http.ListenAndServe(":34973", nil) // listen to all who select the right port
+	address := ":34973"
+	fmt.Printf("server listening on %v ...", address)
+	log.Fatal(http.ListenAndServe(address, handler))
+}
+
+func apiChannelsHandler(response http.ResponseWriter, r *http.Request) {
+	channels, err := svdrp.ListAllChannels("vdr.dd:6419") //get list of all channels
+	if err != nil {
+		fmt.Fprintln(response, "Could not List Channels")
+	}
+	marshChannels, err := json.Marshal(channels)
+	if err != nil {
+		fmt.Fprintln(response, "could not marshal the channel list")
+	}
+	response.Header().Add("Content-type", "application/json")
+	fmt.Fprintln(response, string(marshChannels))
+}
+
+func apiEPGHandler(response http.ResponseWriter, r *http.Request) {
+	epgData, err := svdrp.ListEPGNow("vdr.dd:6419") //get list of all channels
+	if err != nil {
+		fmt.Fprintln(response, "Could not List EPGData")
+	}
+	marshEPG, err := json.Marshal(epgData)
+	if err != nil {
+		fmt.Fprintln(response, "could not marshal the EPGData")
+	}
+	response.Header().Add("Content-type", "application/json")
+	fmt.Fprintln(response, string(marshEPG))
+}
+
+func testHadndler(response http.ResponseWriter, r *http.Request) {
+	testHtml, err := ioutil.ReadFile("html/test.html")
 	if err != nil {
 		fmt.Println("ERROR:", err)
 	}
+
+	fmt.Fprintf(response, string(testHtml))
 }
 
-func startPageHandler(response http.ResponseWriter, r *http.Request) {
-	content := "<h1>VDR Manager</h1>"
-	channels, err := svdrp.ListAllChannels("vdr.dd:6419")
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	content = content + `<div class="alert alert-danger" role="alert">
-	This asdfasdfasdfasdfasdf
-  </div>`
-	content = content + "<ul>"
-	for _, chann := range channels {
-		content = content + fmt.Sprintf("<li>%d - %s</li>", chann.Position, chann.Name)
-	}
-	content = content + "</ul>"
-
-	renderHTML(response, content)
-}
-
-func renderHTML(response http.ResponseWriter, content string) {
-	htmlHeader, err := ioutil.ReadFile("html/header.html")
-	if err != nil {
-		fmt.Fprintln(response, "Could not load header")
-		return
-	}
-	htmlFooter, err := ioutil.ReadFile("html/footer.html")
-	if err != nil {
-		fmt.Fprintln(response, "Could not load footer")
-		return
-	}
-
-	fmt.Fprintln(response, string(htmlHeader)+content+string(htmlFooter))
-
-}
-
-func webshopHandler(response http.ResponseWriter, r *http.Request) {
-	htmlWebshop, err := ioutil.ReadFile("html/webshop.html")
-	if err != nil {
-		fmt.Fprintln(response, "Could not read webshop.html")
-	}
-
-	fmt.Fprintln(response, string(htmlWebshop))
-}
