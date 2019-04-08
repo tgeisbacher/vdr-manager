464dc3b4b6d4512c3a9a83748946146f662982a2
Thomas Geisbacher <thomas.geisbacher@gmail.com>
2019-04-08 11:02:04 +0200

    fixed error-handling

--- main.go
+++ main.go
@@ -53,15 +53,13 @@ func main() {
 func apiChannelsHandler(response http.ResponseWriter, r *http.Request) {
 	channels, err := svdrp.ListAllChannels("vdr.dd:6419") //get list of all channels
 	if err != nil {
-		errResponse := handleAPIError(response, "Could not List Channels", "error while marshaling error-json on channel-listing: %v\n")
-		if errResponse {
-			fmt.Println("return error")
-			return
-		}
+		handleAPIError(response, err, "Could not List Channels", "error while marshaling error-json on channel-listing: %v\n")
+		return
 	}
 	marshChannels, err := json.Marshal(channels)
 	if err != nil {
-		fmt.Fprintln(response, "could not marshal the channel list")
+		handleAPIError(response, err, "could not marshal the channel list", "")
+		return
 	}
 	response.Header().Add("Content-type", "application/json")
 	fmt.Fprintln(response, string(marshChannels))
@@ -70,29 +68,25 @@ func apiChannelsHandler(response http.ResponseWriter, r *http.Request) {
 func apiEPGHandler(response http.ResponseWriter, r *http.Request) {
 	epgData, err := svdrp.ListEPGNow("vdr.dd:6419") //get list of all channels
 	if err != nil {
-		errResponse := handleAPIError(response, "Could not List EPGData", "error while marshaling error-json on EPGNow-listing: %v\n")
-		if errResponse {
-			fmt.Println("return error")
-			return
-		}
+		handleAPIError(response, err, "Could not List EPGData", "error while marshaling error-json on EPGNow-listing: %v\n")
+		return
 	}
 	marshEPG, err := json.Marshal(epgData)
 	if err != nil {
-		fmt.Fprintln(response, "could not marshal the EPGData")
+		handleAPIError(response, err, "could not marshal the EPGData", "")
+		return
 	}
 	response.Header().Add("Content-type", "application/json")
 	fmt.Fprintln(response, string(marshEPG))
 }
 
-func handleAPIError(response http.ResponseWriter, apiErrMassage, jsonErrMassage string) bool {
-	fmt.Println(apiErrMassage)
-	marshErrAPI, jsonErr := json.Marshal(apiResponse{apiErrMassage, nil})
+func handleAPIError(response http.ResponseWriter, err error, apiErrMessage, jsonErrMessage string) {
+	fmt.Printf(apiErrMessage+": %v\n", err)
+	marshErrAPI, jsonErr := json.Marshal(apiResponse{apiErrMessage, nil})
 	if jsonErr != nil {
-		fmt.Printf(apiErrMassage, jsonErr)
+		fmt.Printf(jsonErrMessage, jsonErr)
 		response.WriteHeader(http.StatusInternalServerError)
-		return true
 	}
 	response.Header().Add("Content-type", "application/json")
 	fmt.Fprintln(response, string(marshErrAPI))
-	return true
 }
