a84c4badc447003116c4956c3496b517eaf628c6
Thomas Geisbacher <thomas.geisbacher@gmail.com>
2019-05-21 20:43:24 +0200

    added api wrapper for channels and epg

--- main.go
+++ main.go
@@ -11,18 +11,20 @@ import (
 	"github.com/tgeisbacher/vdr-manager/svdrp"
 )
 
+const vdrHostname = "vdr.dd:6419"
+
 type apiResponse struct {
 	Error string
 	Data  interface{}
 }
 
 func main() {
-	_, err := svdrp.ListAllChannels("vdr.dd:6419")
+	_, err := svdrp.ListAllChannels(vdrHostname)
 	if err != nil {
 		fmt.Println("ERROR:", err)
 	}
 
-	EPGNow, err := svdrp.ListEPGNow("vdr.dd:6419")
+	EPGNow, err := svdrp.ListEPGNow(vdrHostname)
 	if err != nil {
 		fmt.Println("ERROR:", err)
 	}
@@ -33,7 +35,7 @@ func main() {
 		fmt.Println("Starttime:", data.StartTime, "Duration:", data.Duration)
 		fmt.Println("Subtitle: ", data.Subtitle)
 	}
-	_, err = svdrp.ListEPGNow("vdr.dd:6419")
+	_, err = svdrp.ListEPGNow(vdrHostname)
 	if err != nil {
 		fmt.Println("ERROR:", err)
 	}
@@ -51,12 +53,12 @@ func main() {
 }
 
 func apiChannelsHandler(response http.ResponseWriter, r *http.Request) {
-	channels, err := svdrp.ListAllChannels("vdr.dd:6419") //get list of all channels
+	channels, err := svdrp.ListAllChannels(vdrHostname) //get list of all channels
 	if err != nil {
 		handleAPIError(response, err, "Could not List Channels", "error while marshaling error-json on channel-listing: %v\n")
 		return
 	}
-	marshChannels, err := json.Marshal(channels)
+	marshChannels, err := json.Marshal(apiResponse{Error: "", Data: channels})
 	if err != nil {
 		handleAPIError(response, err, "could not marshal the channel list", "")
 		return
@@ -66,12 +68,12 @@ func apiChannelsHandler(response http.ResponseWriter, r *http.Request) {
 }
 
 func apiEPGHandler(response http.ResponseWriter, r *http.Request) {
-	epgData, err := svdrp.ListEPGNow("vdr.dd:6419") //get list of all channels
+	epgData, err := svdrp.ListEPGNow(vdrHostname) //get list of all channels
 	if err != nil {
 		handleAPIError(response, err, "Could not List EPGData", "error while marshaling error-json on EPGNow-listing: %v\n")
 		return
 	}
-	marshEPG, err := json.Marshal(epgData)
+	marshEPG, err := json.Marshal(apiResponse{Error: "", Data: epgData})
 	if err != nil {
 		handleAPIError(response, err, "could not marshal the EPGData", "")
 		return
@@ -82,7 +84,7 @@ func apiEPGHandler(response http.ResponseWriter, r *http.Request) {
 
 func handleAPIError(response http.ResponseWriter, err error, apiErrMessage, jsonErrMessage string) {
 	fmt.Printf(apiErrMessage+": %v\n", err)
-	marshErrAPI, jsonErr := json.Marshal(apiResponse{apiErrMessage, nil})
+	marshErrAPI, jsonErr := json.Marshal(apiResponse{Error: apiErrMessage, Data: nil})
 	if jsonErr != nil {
 		fmt.Printf(jsonErrMessage, jsonErr)
 		response.WriteHeader(http.StatusInternalServerError)
